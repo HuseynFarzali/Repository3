0db3f3bf3e863a2da21b3efdebbc13e85f91b3aa
HuseynFarzali <huseynyf@code.edu.az>
2023-06-13 06:28:14 -0700

    Operator *  overloading added and examples given

--- classes.cpp
+++ classes.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 class Three_Dimensional_Euclidean_Point {
     private:
@@ -11,11 +13,22 @@ class Three_Dimensional_Euclidean_Point {
 
         double Norm() { return sqrt(_r1 * _r1 + _r2 * _r2 + _r3 * _r3); }
 
-        double vectorProduct(Three_Dimensional_Euclidean_Point& p) {
-            double r1 = this->r2 * p._r3 - this->_r3 * p._r2;
+        Three_Dimensional_Euclidean_Point vectorProduct(Three_Dimensional_Euclidean_Point& p) {
+            double r1 = this->_r2 * p._r3 - this->_r3 * p._r2;
             double r2 = -(this->_r1 * p._r3 - this->_r3 * p._r1);
             double r3 = this->_r1 * p._r2 + this->_r2 * p._r1;
 
             return Three_Dimensional_Euclidean_Point(r1, r2, r3);
         }
+
+        double operator* (const Three_Dimensional_Euclidean_Point& p) {
+            return this->_r1 * p._r1 + this->_r2 * p._r2 + this->_r3 * p._r3;
+        }
+};
+
+int main() {
+    Three_Dimensional_Euclidean_Point p(1, 2, 3);
+    Three_Dimensional_Euclidean_Point q(4, 5, 6);
+
+    cout << p * q << endl;
 }
