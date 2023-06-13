d2c66704a525d53b24d1e262867435504160983c
HuseynFarzali <huseynyf@code.edu.az>
2023-06-13 06:22:08 -0700

    Constructer and vectorProduct() function added

--- classes.cpp
+++ classes.cpp
@@ -5,5 +5,17 @@ class Three_Dimensional_Euclidean_Point {
         double _r1, _r2, _r3;
     
     public:
+        Three_Dimensional_Euclidean_Point(double r1, double r2, double r3) {
+            _r1 = r1; _r2 = r2; _r3 = r3;
+        }
+
         double Norm() { return sqrt(_r1 * _r1 + _r2 * _r2 + _r3 * _r3); }
+
+        double vectorProduct(Three_Dimensional_Euclidean_Point& p) {
+            double r1 = this->r2 * p._r3 - this->_r3 * p._r2;
+            double r2 = -(this->_r1 * p._r3 - this->_r3 * p._r1);
+            double r3 = this->_r1 * p._r2 + this->_r2 * p._r1;
+
+            return Three_Dimensional_Euclidean_Point(r1, r2, r3);
+        }
 }
